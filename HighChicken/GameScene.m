4ba06f77716f8a2bf135c8a925523a75032dcbf7
aorzes <anton.orzes@gmail.com>
2015-12-09 21:03:48 +0100

    dodavanje slika

--- HighChicken/GameScene.m
+++ HighChicken/GameScene.m
@@ -7,45 +7,36 @@
 //
 
 #import "GameScene.h"
+#import "igraScena.h"
 
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
-
     
-    
-    SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-    
-    sprite.xScale = 0.5;
-    sprite.yScale = 0.5;
-   // sprite.position = location;
-    
-    SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-    
-    [sprite runAction:[SKAction repeatActionForever:action]];
-    
-    [self addChild:sprite];
+    SKSpriteNode *tipka = [SKSpriteNode spriteNodeWithImageNamed:@"crvenaTipka1"];
+    tipka.xScale = 0.5;
+    tipka.yScale = 0.3;
+    tipka.position = CGPointMake(self.size.width/2, self.size.height - 100);
+    tipka.name = @"start";
+    [self addChild:tipka];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+    CGPoint p = [[touches anyObject]locationInNode:self];
+    SKNode *node = [self nodeAtPoint:p];
+    if ([node.name isEqualToString:@"start"]) {
+        SKScene *igra = [[igraScena alloc]initWithSize:self.size];
+        igra.scaleMode = SKSceneScaleModeAspectFill;
+        SKTransition *tranzicija = [SKTransition pushWithDirection:SKTransitionDirectionDown duration:0.4];
+        [self.view presentScene:igra transition:tranzicija];
+    
+    
     }
+
+
 }
 
 -(void)update:(CFTimeInterval)currentTime {
